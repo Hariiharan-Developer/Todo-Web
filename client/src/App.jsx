f8c3317e2e15e20276e4378e3d1b048ec04eada2
Hariiharan-Developer <hreehs1428@gmail.com>
2026-05-21 14:46:55 +0530

    deleted node module folder mistekn=enly installed outside the scop

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
+import {Routes,Route} from 'react-router-dom'
+import Login from "./component/Login"
+import Register from "./component/Register"
+import Navigation from "./component/Navigation"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Navigation/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
+    </div>
   )
 }
 
